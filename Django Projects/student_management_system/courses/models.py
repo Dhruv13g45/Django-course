0758abd91cdad30869d1dd94b01f5199a0794c78
Dhruv Goradia <dhruvgoradia13@gmail.com>
2025-06-05 15:37:10 +0530

    added views in courses

--- Django Projects/student_management_system/courses/models.py
+++ Django Projects/student_management_system/courses/models.py
@@ -1,17 +1,11 @@
+import datetime
 from django.db import models
 from django.apps import apps
 
 class Courses(models.Model):
-    COURSE_CHOICES = [
-        ('CSE', 'Computer Science Engineering'),
-        ('ME', 'Mechanical Engineering'),
-        ('CE', 'Civil Engineering'),
-        ('IT', 'Information Technology'),
-        ('AIML', 'Artificial Intelligence and Machine Learning'),
-        ('DS', 'Data Science'),
-    ]
-    course_title = models.CharField(max_length=100, choices=COURSE_CHOICES)
-    course_code = models.IntegerField(primary_key=True)
+    course_title = models.CharField(max_length=100)
+    course_code = models.AutoField(primary_key=True)
+    course_desc = models.TextField(default='')
 
     def __str__(self):
         return self.course_title
--- Django Projects/student_management_system/students/views.py
+++ Django Projects/student_management_system/students/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_objects_or_404
+
+from django.shortcuts import render, get_object_or_404,redirect
+
+from courses.models import Courses
 from .models import Student
 
 
@@ -8,20 +11,38 @@ def display_students(request):
 
     return render(request, 'displayAllStudents.html', {'all_students': students})
 
-
 def add_student(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         name = request.POST.get('student_name')
-        course = request.POST.get('student_course')
         email = request.POST.get('student_email')
-        single_student_obj = Student.objects.create(std_name=name,std_email=email,std_course=course)
-    
-    return render(request,'addStudent.html',{'single_student' : single_student_obj})
+        password = request.POST.get('student_password')
 
+        std_obj = Student.objects.create(std_name= name , std_email = email, std_password = password)
+    
+    return render(request,'add_student.html',{'std_profile' : std_obj})
 
 
 def edit_student_profile(request,student_id):
     if request.method == "POST":
-        std_obj = get_objects_or_404(Student,student_id)
+        std_obj = get_object_or_404(Student,student_id)
         name = request.POST.get('student_name')
         email = request.POST.get('student_email')
+
+        std_obj.std_name = name
+        std_obj.std_email = email
+
+        std_obj.save()
+
+    return render(request,'edit_profile.html',{'std_obj':std_obj})
+
+
+def view_single_student(request,student_id):
+    std_obj = get_object_or_404(Student,student_id)
+    return render(request,"view_profile.html",{'std_obj':std_obj})
+
+
+def delete_student_profile(request,student_id):
+    if request.method == 'POST':
+        std_obj = get_object_or_404(Student,student_id)
+        std_obj.delete()
+        return redirect('all_students')

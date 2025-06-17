8497c202e6e067bf591c7b50be24563bdb5ed289
Dhruv Goradia <dhruvgoradia13@gmail.com>
2025-06-17 17:25:34 +0530

    make again with FastAPI

--- Django Projects/College management system/college_management_system/library/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from college_management_system.library.models import Book
-
-# Register your models here.
-admin.site.register(Book)
--- Django Projects/College management system/college_management_system/notice/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from college_management_system.notice.models import Notice
-
-# Register your models here.
-admin.site.register(Notice)
